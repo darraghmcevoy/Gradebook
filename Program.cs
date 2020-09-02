4ccd4928379ced56666b7a397cabde608536ba37
Darragh McEvoy <mcevoy4@hotmail.com>
2020-09-02 22:23:41 +0100

    First changes

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            
+            var numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
+           
+            var result = (0.0);
+            foreach (double number in numbers)
+            {
+               result += number;
+               
+            }
+               System.Console.WriteLine(result);
+
+            
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
@@ -14,6 +26,6 @@ namespace Gradebook
             {
                 Console.WriteLine("Hello!");
             }
-        }
+        }   
     }
 }
