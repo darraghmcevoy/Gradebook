d5dff84ff680bc65297b0e435bc3c8d79fd268fc
Darragh McEvoy <mcevoy4@hotmail.com>
2020-09-03 01:00:48 +0100

    New changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -6,16 +7,17 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
             
-            var numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
-           
+
             var result = (0.0);
-            foreach (double number in numbers)
+            foreach (double number in grades)
             {
                result += number;
-               
             }
-               System.Console.WriteLine(result);
+               result = result / grades.Count;
+               System.Console.WriteLine($"The average grade is {result:N1}");
 
             
             if(args.Length > 0)
