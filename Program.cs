bdaa9b115cbc6b013d3509ccf83ace433d10e6ec
Darragh McEvoy <mcevoy4@hotmail.com>
2020-09-04 18:31:14 +0100

    New classes

--- Program.cs
+++ Program.cs
@@ -7,17 +7,22 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            Book book2 = new Book();
+            book2.AddGrade(90.1);
+            
+            var book = new Book();
+            book.AddGrade(89.1);
+
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
             
-
             var result = (0.0);
             foreach (double number in grades)
             {
                result += number;
             }
                result = result / grades.Count;
-               System.Console.WriteLine($"The average grade is {result:N1}");
+               Console.WriteLine($"The average grade is {result:N1}");
 
             
             if(args.Length > 0)
