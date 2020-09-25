51ef94ae51f906d412bfced686395e49e0d9426f
Darragh McEvoy <mcevoy4@hotmail.com>
2020-09-25 15:52:21 +0100

    C# Fundamentals

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections.Generic;
 
 namespace Gradebook
 {
-    class Book
+    public class Book
     {
         public Book(string name)
         {
@@ -13,8 +14,25 @@ namespace Gradebook
         {
             grades.Add(grade);
         }
+         public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+            foreach (var grade in grades)
+            {
+                result.High = Math.Max(grade,result.High);
+                result.Low = Math.Min(grade, result.Low);
+               result.Average += grade;
+            }
+               result.Average /= grades.Count;
+               
+             return result;
+        }
 
         public List<double> grades = new List<double>();
+
         private string name;
 
     }
---
+++ GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Scott's Grade Book ");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.grades.Add(77.5);
+            var stats = book.GetStatistics();
+            
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"the highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+        }   
+    }
+}
